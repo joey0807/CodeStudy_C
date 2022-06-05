64d0753eb44ea79a711dcbad84f0f0dfa02a2633
joey0807 <82745321+joey0807@users.noreply.github.com>
2022-06-05 12:14:06 +0900

    CodeUP_C

---
+++ CodeStudy_C/1045.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+	long long int a, b;
+	scanf("%lld %lld", &a, &b);
+	printf("%lld\n", a + b);
+	printf("%lld\n", a - b);
+	printf("%lld\n", a * b);
+	printf("%lld\n", a / b);
+	printf("%lld\n", a % b);
+	printf("%.2f", (float)a / b);
+
+}
